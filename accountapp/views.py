cb0556325daf79e203df6dfb1c50b7b32f81364f
ddubi6796 <shinsm6796@gmail.com>
2021-03-30 18:11:58 +0900

    django course 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def hello_world(request):
     #return HttpResponse('안녕하세요 HJSM의 첫번째 View입니다!!')
-    return render(request, 'base.html')
+    return render(request, 'accountapp/helloworld.html')
