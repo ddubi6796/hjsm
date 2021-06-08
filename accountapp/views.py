7b505386262a8e1b2078aa70acf197cb546a20ef
ddubi6796 <shinsm6796@gmail.com>
2021-06-08 23:36:57 +0900

    django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 
 def hello_world(request):
     #return HttpResponse('안녕하세요 HJSM의 첫번째 View입니다!!')
-    return render(request, 'accountapp/helloworld.html')
+
+    #POST와 GET방식 분기
+    if request.method == "POST":
+        return render(request, 'accountapp/helloworld.html', context={'text': 'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!!!'})
