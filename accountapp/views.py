bd21b2113bfdca80a712fe77855623310a836e21
ddubi6796 <shinsm6796@gmail.com>
2021-06-10 20:27:15 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld    #Alt+Ent: auto import
+
 
 def hello_world(request):
     #return HttpResponse('안녕하세요 HJSM의 첫번째 View입니다!!')
 
     #POST와 GET방식 분기
     if request.method == "POST":
-        return render(request, 'accountapp/helloworld.html', context={'text': 'POST METHOD!!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
-        return render(request, 'accountapp/helloworld.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
