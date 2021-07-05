023f8bc883f95f4eac81f50927d4e20da423f5f8
ddubi6796 <shinsm6796@gmail.com>
2021-07-05 21:15:31 +0900

    django course 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"  ##브라우저에서 hello_world에 접근할때 aacountapp:hello_word만으로 접근할 수 잇도록 해줌.
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'), # 함수형 View
+
+    path('create/', AccountCreateView.as_view(), name='create') # 클래스형 View
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,18 +1,20 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
-from accountapp.models import HelloWorld    #Alt+Ent: auto import
+from accountapp.models import HelloWorld    # Alt+Ent: auto import
 
 
 def hello_world(request):
-    #return HttpResponse('안녕하세요 HJSM의 첫번째 View입니다!!')
+    # return HttpResponse('안녕하세요 HJSM의 첫번째 View입니다!!')
 
-    #POST와 GET방식 분기
+    # OST와 GET방식 분기
     if request.method == "POST":
-
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
@@ -24,3 +26,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User  # 장고가 제공하는 기본 유저 클래스
+    form_class = UserCreationForm # model을 사용하기 위하여 장고가 기본 제공하는 form
+    success_url = reverse_lazy('accountapp:hello_world')  # 함수와 클래스간의 import 방식의 차이로 인해 reverse 함수 사용 불가
+    template_name = 'accountapp/create.html'
